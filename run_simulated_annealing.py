769d98dba55fb7ef55ac1be02fdb23be13690761
Phoebe Goldman <pgoldman@sift.net>
2021-08-26 11:40:46 -0400

    actually print simulated annealing result, lmao

--- run_simulated_annealing.py
+++ run_simulated_annealing.py
@@ -1,15 +1,19 @@
 import analysis.simulated_annealing as sa
 import analysis.metrics as metrics
 import logging
+import pickle
 
 ACCEPTABLE_GROUP_SIZE = 256
-ITERATIONS = 512  # i guess?
+ITERATIONS = 128
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING)
     sa.log.setLevel(logging.INFO)
-    sa.optimize(
+    res = sa.optimize(
         metric=metrics.entropy_metric,
         opt_iters=ITERATIONS,
         group_size=ACCEPTABLE_GROUP_SIZE,
     )
+    with open("simulated_annealing_result.pickle", "wb") as out:
+        pickle.dump(res, out)
+    log.info("optimizer settled on {res}")
