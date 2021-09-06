5bc9cc80389ab4ded4435a84e9c34931eb250142
Jack Maloney <git@jmmaloney4.xyz>
2021-09-06 02:46:04 -0500

    Add axis labels to plots

--- plot.py
+++ plot.py
@@ -15,8 +15,7 @@ from multiprocessing import Pool
 
 log = logging.getLogger(__name__)
 
-GROUP_SIZE = 4
-
+GROUP_SIZE = 32
 def run_group(cards):
     decks = ac.possible_decks(3, cards)
     return sampling.group_tournament(
@@ -87,8 +86,8 @@ def makeplot(data, name, title="Plot 2D array", xaxis=None, yaxis=None):
     fig = plt.figure()
     plt.pcolormesh(np.array(data), cmap='plasma')
     plt.title(title)
-    plt.ylabel(xaxis)
-    plt.xlabel(yaxis)
+    plt.xlabel(xaxis)
+    plt.ylabel(yaxis)
     plt.colorbar()
     fig.savefig(name)
 
@@ -99,15 +98,25 @@ if __name__ == "__main__":
     grow_on_damage = colormap([tourney.BEAR, tourney.TANK, tourney.BRUISER, tourney.EXPLODE_ON_DEATH, tourney.RAMPAGE, tourney.FRIENDLY_VAMPIRE], var1_card=tourney.GROW_ON_DAMAGE)
     with open(f"{sys.argv[1]}/grow_on_damage.pickle", "wb") as pickleout:
         pickle.dump(grow_on_damage, pickleout)
-    makeplot(grow_on_damage, f"{sys.argv[1]}/grow_on_damage.png")
+    makeplot(grow_on_damage, f"{sys.argv[1]}/grow_on_damage.png", title="Grow on Damage", xaxis="Health", yaxis="Attack")
     
     explode_on_death = colormap([tourney.BEAR, tourney.TANK, tourney.BRUISER, tourney.GROW_ON_DAMAGE, tourney.RAMPAGE, tourney.FRIENDLY_VAMPIRE], var1_card=tourney.EXPLODE_ON_DEATH)
     with open(f"{sys.argv[1]}/explode_on_death.pickle", "wb") as pickleout:
         pickle.dump(explode_on_death, pickleout)
-    makeplot(explode_on_death, f"{sys.argv[1]}/explode_on_death.png")
+    makeplot(explode_on_death, f"{sys.argv[1]}/explode_on_death.png", title="Explode on Death", xaxis="Health", yaxis="Attack")
 
-    bruiser_vs_grow = colormap([tourney.BEAR, tourney.TANK, tourney.BRUISER, tourney.RAMPAGE, tourney.FRIENDLY_VAMPIRE, tourney.EXPLODE_ON_DEATH], var1_card=tourney.BRUISER, var1_key=set_atk, var2_card=tourney.GROW_ON_DAMAGE, var2_key=set_health)
+    bruiser_vs_grow = colormap([tourney.BEAR, tourney.TANK, tourney.RAMPAGE, tourney.FRIENDLY_VAMPIRE, tourney.EXPLODE_ON_DEATH], var1_card=tourney.BRUISER, var1_key=set_atk, var2_card=tourney.GROW_ON_DAMAGE, var2_key=set_health)
     with open(f"{sys.argv[1]}/bruiser_vs_grow.pickle", "wb") as pickleout:
         pickle.dump(bruiser_vs_grow, pickleout)
-    makeplot(bruiser_vs_grow, f"{sys.argv[1]}/bruiser_vs_grow.png")
+    makeplot(bruiser_vs_grow, f"{sys.argv[1]}/bruiser_vs_grow.png", title="Bruiser Attack vs Grow on Damage Health", xaxis="Health", yaxis="Attack")
+
+    bear_vs_grow = colormap([tourney.TANK, tourney.BRUISER, tourney.RAMPAGE, tourney.FRIENDLY_VAMPIRE, tourney.EXPLODE_ON_DEATH], var1_card=tourney.BEAR, var1_key=set_atk, var2_card=tourney.GROW_ON_DAMAGE, var2_key=set_health)
+    with open(f"{sys.argv[1]}/bear_vs_grow.pickle", "wb") as pickleout:
+        pickle.dump(bear_vs_grow, pickleout)
+    makeplot(bear_vs_grow, f"{sys.argv[1]}/bear_vs_grow.png", title="Bear Attack vs Grow on Damage Health", xaxis="Health", yaxis="Attack")
+
+    bruiser_vs_explode = colormap([tourney.BEAR, tourney.TANK, tourney.RAMPAGE, tourney.FRIENDLY_VAMPIRE, tourney.GROW_ON_DAMAGE], var1_card=tourney.BRUISER, var1_key=set_atk, var2_card=tourney.EXPLODE_ON_DEATH, var2_key=set_health)
+    with open(f"{sys.argv[1]}/bruiser_vs_explode.pickle", "wb") as pickleout:
+        pickle.dump(bruiser_vs_explode, pickleout)
+    makeplot(bruiser_vs_explode, f"{sys.argv[1]}/bruiser_vs_explode.png", title="Bruiser Attack vs Explode on Death Health", xaxis="Health", yaxis="Attack")
 
