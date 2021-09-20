47421c9934af43726e65a0b8eb7803e2d46b9692
Phoebe Goldman <phoebe@goldman-tribe.org>
2021-09-20 19:52:53 -0400

    fix syntax error forgotten :

--- group_vs_rr.py
+++ group_vs_rr.py
@@ -19,7 +19,8 @@ def compare_sampling(cards: list[ac.Card], deck_size=3) -> None:
         with open(subdir + "/trial_" + str(trial_number), "wb") as outfile:
             pickle.dump(list(data), outfile)
 
-    for grp_frac in [2, 3, 4, 6, 12] # some of the more interesting factor
+    for grp_frac in [2, 3, 4, 6, 12]:
+        # some of the more interesting factor
         group_size = 1788 / grp_frac
 
         subdir = "group_v_rr_res/group_size_" + str(group_size)
